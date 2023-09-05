718b9c2c516c31617fa048d14d416e919ce5445f
faraz-github <farazahmad.email@gmail.com>
2023-09-05 23:57:57 +0530

    ES modules in Nodejs

---
+++ main.mjs
@@ -0,0 +1,12 @@
+// import add from "./math-esm.mjs";
+// import math from "./math-esm.mjs";
+// import * as math from "./math-esm.mjs";
+import { add, subtract } from "./math-esm.mjs";
+
+// const {add, subtract} = math;
+
+// console.log(add(5, 5));
+// console.log(math.add(5, 5));
+// console.log(math.subtract(5, 5));
+console.log(add(5, 5));
+console.log(subtract(5, 5));
---
+++ math-esm.mjs
@@ -0,0 +1,24 @@
+// const add = (a, b) => {
+//   return a + b;
+// };
+
+export const add = (a, b) => {
+  return a + b;
+};
+
+// export default (a, b) => {
+//   return a + b;
+// };
+
+// const subtract = (a, b) => {
+//   return a - b;
+// };
+export const subtract = (a, b) => {
+  return a - b;
+};
+
+// export default add;
+// export default {
+//   add: add,
+//   subtract: subtract,
+// };
