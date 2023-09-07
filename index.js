ca7f337223e0fabc955d21d0192623531ccc0648
faraz-github <farazahmad.email@gmail.com>
2023-09-07 23:19:31 +0530

    Built-In module - path

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
+// path - built-in module
+// const path = require("node:path");
+const path = require("path");
+
+console.log(__filename);
+console.log(__dirname);
+
+console.log(path.basename(__filename));
+console.log(path.basename(__dirname));
+
+console.log(path.extname(__filename));
+console.log(path.extname(__dirname));
+
+console.log(path.parse(__filename));
+console.log(path.format(path.parse(__filename)));
+
+console.log(path.isAbsolute(__filename));
+console.log(path.isAbsolute("./data.json"));
+
+console.log(path.join("/folder1", "//folder2", "index.html"));
+console.log(path.join("/folder1", "//folder2", "../index.html"));
+console.log(path.join(__dirname, "data.json"));
+console.log("====================================");
+console.log(path.resolve("folder1", "folder2", "index.html"));
+console.log(path.resolve("/folder1", "folder2", "index.html"));
+console.log(path.resolve("/folder1", "//folder2", "index.html"));
+console.log(path.resolve("/folder1", "//folder2", "../index.html"));
+console.log(path.resolve(__dirname, "data.json"));
+
 const data = require("./data.json");
 // By default nodejs will parse json data into javascript object
 console.log(data);
